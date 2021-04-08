d52f069a401a613184871e8cebf98ff0eb844ba2
Tri Vo <vocaotri445@gmail.com>
2021-04-08 10:39:21 +0700

    fix-bug-room_id

--- server.js
+++ server.js
@@ -12,45 +12,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/consumer/:roomID", async (req, res) => {
-    const peer = new webrtc.RTCPeerConnection({
-        iceServers: [
-            {
-                urls: "stun:stun.stunprotocol.org"
-            }
-        ]
-    });
-    var roomID = parseInt(req.params.roomID);
-    const desc = new webrtc.RTCSessionDescription(req.body.sdp);
-    await peer.setRemoteDescription(desc);
-    senderStream[roomID].getTracks().forEach(track => peer.addTrack(track, senderStream[roomID]));
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-    const payload = {
-        sdp: peer.localDescription
-    }
+    if (req.params) {
+        const peer = new webrtc.RTCPeerConnection({
+            iceServers: [
+                {
+                    urls: "stun:stun.stunprotocol.org"
+                }
+            ]
+        });
+        var roomID = parseInt(req.params.roomID);
+        const desc = new webrtc.RTCSessionDescription(req.body.sdp);
+        await peer.setRemoteDescription(desc);
+        senderStream[roomID].getTracks().forEach(track => peer.addTrack(track, senderStream[roomID]));
+        const answer = await peer.createAnswer();
+        await peer.setLocalDescription(answer);
+        const payload = {
+            sdp: peer.localDescription
+        }
 
-    res.json(payload);
+        res.json(payload);
+    }
+    else {
+        res.json({ error: "no room id" });
+    }
 });
 
 app.post('/broadcast/:roomID', async (req, res) => {
-    const peer = new webrtc.RTCPeerConnection({
-        iceServers: [
-            {
-                urls: "stun:stun.stunprotocol.org"
-            }
-        ]
-    });
-    var roomID = parseInt(req.params.roomID);
-    peer.ontrack = (e) => handleTrackEvent(e, peer, roomID);
-    const desc = new webrtc.RTCSessionDescription(req.body.sdp);
-    await peer.setRemoteDescription(desc);
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-    const payload = {
-        sdp: peer.localDescription
+    if (req.params) {
+        const peer = new webrtc.RTCPeerConnection({
+            iceServers: [
+                {
+                    urls: "stun:stun.stunprotocol.org"
+                }
+            ]
+        });
+        var roomID = parseInt(req.params.roomID);
+        peer.ontrack = (e) => handleTrackEvent(e, peer, roomID);
+        const desc = new webrtc.RTCSessionDescription(req.body.sdp);
+        await peer.setRemoteDescription(desc);
+        const answer = await peer.createAnswer();
+        await peer.setLocalDescription(answer);
+        const payload = {
+            sdp: peer.localDescription
+        }
+
+        res.json(payload);
+    } else {
+        res.json({ error: "no room id" });
     }
 
-    res.json(payload);
 });
 
 function handleTrackEvent(e, peer, roomID) {
