9e73183b1d479557f15784b26a3a85e7c439802f
Tri Vo <vocaotri445@gmail.com>
2021-04-14 10:27:13 +0700

    update app resolution

--- public/index.js
+++ public/index.js
@@ -1,5 +1,51 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const resolution = {
+    QQVGA: {
+        width: { ideal: 160 },
+        height: { ideal: 120 },
+    },
+    QVGA: {
+        width: { ideal: 320 },
+        height: { ideal: 240 },
+    },
+    WQVGA: {
+        width: { ideal: 428 },
+        height: { ideal: 240 },
+    },
+    VGA: {
+        width: { ideal: 640 },
+        height: { ideal: 480 },
+    },
+    FWVGA: {
+        width: { ideal: 854 },
+        height: { ideal: 480 },
+    },
+    SVGA: {
+        width: { ideal: 832 },
+        height: { ideal: 624 },
+    },
+    QHD: {
+        width: { ideal: 960 },
+        height: { ideal: 540 },
+    },
+    HD: {
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
+    },
+    FullHD: {
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+    },
+    TwoK: {
+        width: { ideal: 2560 },
+        height: { ideal: 1440 },
+    },
+    FourK: {
+        width: { ideal: 3840 },
+        height: { ideal: 2160 },
+    }
+}
 window.onload = () => {
     document.getElementById('my-button').onclick = () => {
         init();
@@ -14,8 +60,7 @@ async function init() {
     stream.getTracks().forEach(track => peer.addTrack(track, stream));
 }
 async function getCamera(device = true) {
-    let width = { ideal: 1920 };
-    let height = { ideal: 1080 };
+    let { width, height } = resolution.QHD
     return navigator.mediaDevices.getUserMedia({
         video: typeof device === "boolean" ? device : { ...device, width: width, height: height },
         audio: true
@@ -29,17 +74,13 @@ async function getConnectedDevices(kind = 'videoinput') {
 async function getDeviceIdCamera(key = 0) {
     let listCamera = await getConnectedDevices();
     await listCamera.forEach((item) => {
-        if (item.deviceId != null && item.deviceId != '')
-            cameraDeviceId.push({ diviceId: item.deviceId, label: item.label });
+        if (item.deviceId != null && item.deviceId != '') cameraDeviceId.push({ diviceId: item.deviceId, label: item.label });
     });
 }
 
 function createPeer() {
     const peer = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: "stun:stun.stunprotocol.org"
-            }
+        iceServers: [{ urls: "stun:stun.stunprotocol.org" }
         ]
     });
     peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
